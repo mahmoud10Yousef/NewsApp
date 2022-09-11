82f87173f8807df5cb6348165e156a8c7870b4c6
mahmoud yousef <36968283+mahmoud10Yousef@users.noreply.github.com>
2022-09-11 12:21:09 -0700

    update search view model

--- NewsApp/ViewModel/SearchViewModel.swift
+++ NewsApp/ViewModel/SearchViewModel.swift
@@ -1,40 +1,40 @@
-//
-//  SearchViewModel.swift
-//  NewsApp
-//
-//  Created by mahmoud yousef on 10/09/2022.
-//
-
 import Foundation
 import RxSwift
 import RxRelay
 
-class SearchViewModel{
+
+class SearchViewModel {
     
-    var textBehavior          = BehaviorRelay<String>(value: "")
-    var loadingBehavior       = BehaviorRelay<Bool>(value: false)
-    var searchingNewsSubjects = PublishSubject<[Article]>()
+    private  var newsModelSubject    = PublishSubject<[Article]>()
+    private  var isTableHidden       = BehaviorRelay<Bool>(value: false)
+    var searchValueBehavior          = BehaviorSubject<String?>(value: "")
+    var loadingBehavior              = BehaviorRelay<Bool>(value: false)
     
-    var isTextValid: Observable<Bool> {
-        return textBehavior.asObservable().map {(text) -> Bool in
-            return text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
-        }
+    var newsSubjects: Observable<[Article]> { Observable.combineLatest(
+        searchValueBehavior
+            .map { $0 ?? "" }
+            .startWith("")
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance),
+        newsModelSubject
+    )
+            .map { searchValue, books in
+                searchValue.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? books : books.filter { $0.title.lowercased().contains(searchValue.lowercased()) }
+            }
     }
     
     
     func getSearchingNews(){
         self.loadingBehavior.accept(true)
-        NetworkManager.shared.fetchData(url: "https://newsapi.org/v2/everything?q=\(textBehavior.value)&apiKey=db71d97193dd453db7fec98cd40a1290"
+        NetworkManager.shared.fetchData(url: "https://newsapi.org/v2/everything?q=bitcoin&apiKey=db71d97193dd453db7fec98cd40a1290"
                                         , decodable: News.self) { [weak self] result in
             self?.loadingBehavior.accept(false)
             switch result{
                 
             case .success(let news):
-                self?.searchingNewsSubjects.onNext(news.articles)
+                self?.newsModelSubject.onNext(news.articles)
             case .failure(let error):
-                self?.searchingNewsSubjects.onError(error.rawValue as! Error)
+                print(error.localizedDescription)
             }
-            
         }
     }
 }
